c9b198d4ae2c9ace7dee943934ab3294f04560bd
Darbley <stapleton.b@gmail.com>
2022-08-31 17:19:50 -0400

    adjust link button

--- components/UI/LinkButton/LinkButton.js
+++ components/UI/LinkButton/LinkButton.js
@@ -2,11 +2,14 @@ import Link from 'next/link';
 import cssClass from './linkbutton.module.scss';
 
 const LinkButton = (props) => {
-
-    return (
-        <button>
-            <Link href={props.href} ><a className={cssClass.btn}>{props.children}</a></Link>
-        </button>
-    )
+    if(props.href){
+        return (
+            <button>
+                <Link href={props.href} ><a className={cssClass.btn}>{props.children}</a></Link>
+            </button>
+        )
+    }
+    return <button className={cssClass.btn} onClick={props.onClick}>{props.children}</button>
+    
 }
 export default LinkButton;
